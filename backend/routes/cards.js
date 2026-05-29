73cf397de913eccc01a231f05d1f5dfea3c0104c
Abel <ajdszp@hotmail.com>
2026-05-29 14:09:33 +0100

    fix; error cards routes

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -6,7 +6,7 @@ const cardController = require('../controllers/cardController');
 
 router.get('/',           cardController.getAll);
 router.get('/by-client', cardController.getByClient);
-router-get('/:id',        cardController.getById);
+router.get('/:id',        cardController.getById);
 router.post('/',          cardController.create);
 router.put('/:id',        cardController.update);
 router.patch('/:id/status', cardController.updateStatus);
