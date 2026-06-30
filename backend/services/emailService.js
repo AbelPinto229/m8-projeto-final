3b0cbad64a36d33725003b5a0b4ad5bfa6f084be
Abel Pinto <ajdszp@hotmail.com>
2026-06-30 10:34:24 +0100

    fix: oulookt smtp

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,16 +1,18 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp-mail.outlook.com',
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
 const sendOTP = async (to, otp) => {
   await transporter.sendMail({
-    from: `"OffScroll" <${process.env.EMAIL_USER}>`,
+    from: `"OffScroll" <${process.env.SMTP_USER}>`,
     to,
     subject: 'Recuperar password — OffScroll',
     html: `
